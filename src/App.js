4f308711c753f2282ccbd894c9c453652ad4bcd2
Karan Vani <karan.s.vani@gmail.com>
2024-01-09 16:58:57 +0000

    removed content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Content from "./components/content";
 
 function App() {
   return (
